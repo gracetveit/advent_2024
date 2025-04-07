adb3c9efefa9b6cc1db5b49fe0006541de24475f
Grace <grace.tveit@gmail.com>
2025-04-07 15:51:45 -0700

    updating main for part two

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 pub mod input_reader;
 mod solutions;
-use solutions::day_01::solution::day_01_solution as solution_fn;
+use solutions::day_01::solution::part_two as solution_fn;
 
 fn main() {
     let solution = solution_fn("inputs/day_01.txt");
