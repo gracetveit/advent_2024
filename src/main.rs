791cef7cc3bc45acd1843bee1dac7e6c3f302a28
Grace <grace.tveit@gmail.com>
2025-04-07 15:58:20 -0700

    adding all solutions to main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 pub mod input_reader;
 mod solutions;
-use solutions::day_01::solution::part_two as solution_fn;
 
 fn main() {
-    let solution = solution_fn("inputs/day_01.txt");
-    println!("{solution:}");
+    println!("# Day 1\n");
+    println!(
+        "Part 1: {:}",
+        solutions::day_01::solution::day_01_solution("inputs/day_01.txt")
+    );
+    println!(
+        "Part 2: {:}",
+        solutions::day_01::solution::part_two("inputs/day_01.txt")
+    );
 }
