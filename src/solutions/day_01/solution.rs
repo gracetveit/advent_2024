92467e48a1d12596e0d611d7fefdeb88e3cfe070
Grace <grace.tveit@gmail.com>
2025-04-07 12:50:03 -0700

    adding approach

--- src/solutions/day_01/solution.rs
+++ src/solutions/day_01/solution.rs
@@ -17,6 +17,19 @@
 /// to *add up all of those distances.* For example, if you pair up a `3` from
 /// the left list with a `7` from the right list, the distance apart is `4`;
 /// if you pair up a `9` with a `3`, the distance apart is `6`.
+/// 
+/// # Approach
+/// First, we need to read the input file. Using the `input_reader.rs`, we can
+/// get a raw string. We can split the string by `\n`, and then seperately add
+/// the first and last item from the sub-string to two seperate vectors, and 
+/// then return those vectors.
+/// 
+/// Next we need to sort the vectors from smallest to largest, using one of
+/// Rust's buit-in tools.
+/// 
+/// After we've sorted the vectors, maybe we can `reduce` them. First
+/// finding the absolute value between the two, and then adding that to a
+/// rolling sum.
 pub fn sol() {
     println!("Hello World!");
 }
