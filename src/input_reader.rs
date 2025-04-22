b0921864693ddd1b6de0a91cf54d3b91eb02e6cf
Grace Tveit <grace.tveit@gmail.com>
2025-04-22 09:34:46 -0700

    Simplifying error handling

--- src/input_reader.rs
+++ src/input_reader.rs
@@ -1,13 +1,5 @@
 use std::fs;
 
 pub fn input_reader(filename: String) -> String {
-    let contents = fs::read_to_string(filename);
-    match contents {
-        Ok(read_contents) => {
-            return read_contents;
-        }
-        Err(error) => {
-            panic!("{error:?}");
-        }
-    }
+    return fs::read_to_string(filename).expect("File read error!");
 }
